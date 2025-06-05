0af2d71e6c8274f6efc2ebc86673f996cdb95843
Gábor Torma <gabor@torma.dev>
2025-06-05 16:40:52 +0200

    fix: imports

--- src/events.ts
+++ src/events.ts
@@ -1,26 +1,27 @@
-import * as vscode from 'vscode'
+import type { ExtensionContext } from 'vscode'
+import { commands, window } from 'vscode'
 import { isEnabled } from './settings'
 
 let isAutoHidden = false
 
 function toggleAuxiliaryBar() {
-  if (!isEnabled() || !vscode.window.visibleTextEditors?.length)
+  if (!isEnabled() || !window.visibleTextEditors?.length)
     return
 
-  if (vscode.window.visibleTextEditors?.length > 1) {
-    vscode.commands.executeCommand('workbench.action.closeAuxiliaryBar')
+  if (window.visibleTextEditors?.length > 1) {
+    commands.executeCommand('workbench.action.closeAuxiliaryBar')
     isAutoHidden = true
   }
   else if (isAutoHidden) {
-    vscode.commands.executeCommand('workbench.action.focusAuxiliaryBar').then(
-      () => vscode.commands.executeCommand('workbench.action.focusActiveEditorGroup'),
+    commands.executeCommand('workbench.action.focusAuxiliaryBar').then(
+      () => commands.executeCommand('workbench.action.focusActiveEditorGroup'),
     )
     isAutoHidden = false
   }
 }
 
-export function registerEvents(context: vscode.ExtensionContext): void {
-  vscode.window.onDidChangeActiveTextEditor(toggleAuxiliaryBar, null, context.subscriptions)
+export function registerEvents(context: ExtensionContext): void {
+  window.onDidChangeActiveTextEditor(toggleAuxiliaryBar, null, context.subscriptions)
 
   toggleAuxiliaryBar()
 }
--- src/items.ts
+++ src/items.ts
@@ -1,9 +1,9 @@
-import * as vscode from 'vscode'
+import { StatusBarAlignment, window, workspace } from 'vscode'
 import { COMMAND_NAME, DEFAULT_STATUS_BAR_TEXT_CONFIGURATION } from './consts'
 import { isEnabled } from './settings'
 
 function createToggleAuxiliaryBarStatusBarItem() {
-  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1)
+  const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 1)
   statusBarItem.command = 'workbench.action.toggleAuxiliaryBar'
   statusBarItem.tooltip = 'Toggle Auxiliary Bar'
   statusBarItem.text = `$(list-selection)`
@@ -17,13 +17,13 @@ function getToggleEnableFeatureStatusBarText() {
 }
 
 function createToggleEnableFeatureStatusBarItem() {
-  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 2)
+  const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 2)
   statusBarItem.command = COMMAND_NAME
   statusBarItem.tooltip = 'Toggle Auto Hide Auxiliary Bar'
   statusBarItem.text = getToggleEnableFeatureStatusBarText()
   statusBarItem.show()
 
-  vscode.workspace.onDidChangeConfiguration(() => {
+  workspace.onDidChangeConfiguration(() => {
     statusBarItem.text = getToggleEnableFeatureStatusBarText()
   })
 }
